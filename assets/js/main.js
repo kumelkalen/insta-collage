2a09c000464e7f25c03675668e5bcaf3c9322cd5
kumelkalen <free.your.mind.fc@gmail.com>
2017-06-16 00:37:55 -0400

    Creamos la segunda sección

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,47 +1,18 @@
-$('#loginForm').bootstrapValidator({
- 
-	 message: 'Este valor no es valido',
- 
-	 feedbackIcons: {
- 
-		 valid: 'glyphicon glyphicon-ok',
- 
-		 invalid: 'glyphicon glyphicon-remove',
- 
-		 validating: 'glyphicon glyphicon-refresh'
- 
-	 },
- 
-	 fields: {
- 
-		 usuario: {
- 
-			 validators: {
- 
-				 notEmpty: {
- 
-					 message: 'El nombre de usuario es requerido'
- 
-				 }
- 
-			 }
- 
-		 },
- 
-		 password: {
- 
-			 validators: {
- 
-				 notEmpty: {
- 
-					 message: 'La contraseña es requerida'
- 
-				 }
- 
-			 }
- 
-		 }
- 
-	 }
- 
-});
+// Función para arrastrar -ondragstart-
+function drag(ev) {
+  console.log(ev.target.src); // Verificamos en la consola lo que ocurre
+  ev.dataTransfer.setData('text', ev.target.id); // Indicamos el tipo de dato que ingresaremos
+}
+
+// Inicializamos el drop -ondragover-
+function permitirDrop(ev){
+  ev.preventDefault(); // Evitamos que el navegador manipule nuestros datos
+}
+
+// Obtenemos en una variable el elemento arrastrado
+function drop(ev) {
+  ev.preventDefault();
+
+  var id_foto = ev.dataTransfer.getData('text'); // Con getData tomamos el elemento
+  ev.target.appendChild(document.getElementById(id_foto)); // Agregamos el elemento arrastrado al elemento en el que se produjo el evento 
+}
